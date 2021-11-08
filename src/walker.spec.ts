2d6200eef3df5c633234bc9aa4fa8c2830fa0be3
Mischback <dev@mischback.de>
2021-11-08 10:52:06 +0100

    Implement tests for rejections caused by fs operations

--- src/walker.spec.ts
+++ src/walker.spec.ts
@@ -8,17 +8,66 @@ jest.mock("fs/promises");
 jest.mock("path");
 
 /* import the subject under test (SUT) */
-// import { fileObjectWalker, FileObjectWalkerError } from "./walker";
+import { fileObjectWalker, FileObjectWalkerError } from "./walker";
 
 /* additional imports */
-// import { readdir, stat } from "fs/promises";
-// import { resolve } from "path";
+import { Stats } from "fs";
+import { readdir, stat } from "fs/promises";
+import { resolve } from "path";
 
 describe("fileObjectWalker()...", () => {
   it("...rejects with a FileObjectWalkerError if stat() fails", () => {
-    expect(1).toBe(1);
+    /* define the parameter */
+    const testRejection = "foobar";
+    const testStartingPoint = "testEntryPoint";
+    const testPayload = (
+      _startingPoint: string,
+      _payloadConfig: unknown
+    ): Promise<void> => {
+      return Promise.resolve();
+    };
+
+    /* setup mocks and spies */
+    (resolve as jest.Mock).mockReturnValue(testStartingPoint);
+    (stat as jest.Mock).mockRejectedValue(testRejection);
+
+    /* make the assertions */
+    return fileObjectWalker(testStartingPoint, testPayload).catch(
+      (err: FileObjectWalkerError) => {
+        expect(err).toBeInstanceOf(FileObjectWalkerError);
+        expect(err.message).toBe(`Could not stat() "${testStartingPoint}"`);
+        expect(err.originalError).toBe(testRejection);
+      }
+    );
   });
+
   it("...rejects with a FileObjectWalkerError if readdir() fails", () => {
-    expect(1).toBe(1);
+    /* define the parameter */
+    const testRejection = "foobar";
+    const testStartingPoint = "testEntryPoint";
+    const testStatObject = new Stats();
+    testStatObject.isDirectory = () => {
+      return true;
+    };
+    const testPayload = (
+      _startingPoint: string,
+      _payloadConfig: unknown
+    ): Promise<void> => {
+      return Promise.resolve();
+    };
+
+    /* setup mocks and spies */
+    (resolve as jest.Mock).mockReturnValue(testStartingPoint);
+    (stat as jest.Mock).mockResolvedValue(testStatObject);
+    (readdir as jest.Mock).mockRejectedValue(testRejection);
+
+    /* make the assertions */
+    return fileObjectWalker(testStartingPoint, testPayload).catch(
+      (err: FileObjectWalkerError) => {
+        expect(err).toBeInstanceOf(FileObjectWalkerError);
+        expect(err.message).toBe(`Could not readdir() "${testStartingPoint}"`);
+        expect(err.originalError).toBe(testRejection);
+      }
+    );
   });
 });
